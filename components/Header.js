aec5f66dc5d94b79e2611bc35e612698288b0ac5
hasindusithmin <hasindusithmin64@gmail.com>
2022-12-30 20:14:15 +0530

    Update pages/crypto/[coin].js

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,5 @@
 
-
+import Link from "next/link"
 export default function Header() {
     return (
         <header className="w3-container w3-theme w3-padding-large">
@@ -7,6 +7,9 @@ export default function Header() {
                 <h4>Easily Download Cryptocurrency Market Data</h4>
                 <h1 className="w3-xxxlarge w3-animate-bottom">TRADE TRACKER</h1>
                 <p className="w3-hide-small">Get instant access to a wide range of market data, including recent trades, aggregate trades, candlestick data, and more.</p>
+                <p>
+                    <Link href="/" className="w3-button w3-padding w3-grey w3-text-white"><b>HOME</b></Link>
+                </p>
             </div>
         </header>
     )
