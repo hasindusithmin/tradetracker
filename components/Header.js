8cc2a67584a9f86593efcd0ec76960a1f7dd55d8
hasindusithmin <hasindusithmin64@gmail.com>
2022-12-30 18:39:03 +0530

    Create components/Header.js

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+
+
+export default function Header() {
+    return (
+        <header className="w3-container w3-theme w3-padding-large">
+            <div className="w3-center w3-padding-large">
+                <h4>Easily Download Cryptocurrency Market Data</h4>
+                <h1 className="w3-xxxlarge w3-animate-bottom">TRADE TRACKER</h1>
+                <p className="w3-hide-small">Get instant access to a wide range of market data, including recent trades, aggregate trades, candlestick data, and more.</p>
+            </div>
+        </header>
+    )
+}
